bf9fd9335c4b9c18245f11fb365f609301fe0186
Andre2610 <dre2610@gmail.com>
2020-10-31 14:48:16 +0100

    apollo server running

---
+++ graphql/typeDefs.js
@@ -0,0 +1,33 @@
+const { gql } = require("apollo-server")
+
+module.exports = gql`
+    type Post {
+        id: ID!,
+        body: String!,
+        userName: String!,
+        createdAt: String!,
+    }
+
+    type User {
+        id: ID!,
+        email: String!,
+        token: String!,
+        userName: String!,
+        createdAt: String!
+    }
+
+    input RegisterInput {
+        userName: String!,
+        password: String!,
+        confirmPassword: String!,
+        email: String!
+    }
+
+    type Query {
+        getPosts: [Post]
+    }
+
+    type Mutation {
+        register(registerInput: RegisterInput): User!
+    }
+`
